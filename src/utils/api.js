ec3b390fc3c254798e049c71bee532848b41eef3
umenlu <550987859@qq.com>
2018-12-29 08:31:22 +0000

    增加详情页面

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import wepy from 'wepy'
 
 // 服务器接口地址
-const host = 'http://larabbs.test/api'
+// const host = 'http://larabbs.test/api'
+const host = 'https://qiyuan.umenlu.com/api'
 
 // 普通请求
 const request = async (options, showLoading = true) => {
